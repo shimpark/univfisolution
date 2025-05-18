19c44f31a3549c75e49174a5ed177a28323f8f3b
shimpark <shimpark@gmail.com>
2025-05-19 02:05:03 +0900

    불필요 삭제

--- UnivFI.WebUI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace UnivFI.WebUI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
